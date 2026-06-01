9eb6bf79e39ae40b911f9b50aa54cdf88c556146
Guilherme <guivasbaldesin@gmail.com>
2026-06-01 06:16:07 -0300

    app finalizado 1/2 falta polimento.

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -6,6 +6,9 @@ import { Screen } from "@/components/Screen"
 import { FAB } from 'react-native-paper';
 import { useAppTheme } from "@/theme/context"
 import { ThemedStyle } from "@/theme/types"
+import { Text } from "@/components/Text"
+import { loadString, saveString } from "@/utils/storage";
+
 interface HomeScreenProps extends AppStackScreenProps<"Home"> { }
 
 export const HomeScreen: FC<HomeScreenProps> = () => {
@@ -13,49 +16,144 @@ export const HomeScreen: FC<HomeScreenProps> = () => {
 
   const [acaoUsuario, setAcaoUsuario] = useState('leitura');
   const [conteudo, setConteudo] = useState('');
+  const [flag, setFlag] = useState(0);
   const [lines, setLines] = useState(25);
   const [newHeight, setNewHeight] = useState(0)
   const inputRef = useRef<TextInput>(null);
-
   const LINES = 25
 
+
   useEffect(() => {
-    console.log(lines)
-  }, [lines])
+    const contentPersistent = loadString('conteudo')
+    if (contentPersistent) {
+      setConteudo(contentPersistent)
+    }
+
+  }, [])
+  useEffect(() => {
+    //salvamento do conteúdo
+    if (acaoUsuario == 'leitura' && conteudo.length >= 0 && flag)
+      saveString('conteudo', conteudo)
+
+
+  }, [acaoUsuario, conteudo.length >= 0])
+
+
+  const handleFAB = () => {
+    if (acaoUsuario == 'leitura') {
+      setAcaoUsuario('escrita')
+      setFlag(0)
+    }
+    else {
+      setAcaoUsuario('leitura')
+      setFlag(1)
+
+    }
+
+
+
+
+
+  }
+
+
 
   return (
     <Screen style={themed($root)} contentContainerStyle={{ flex: 1 }} preset="fixed">
-      {/* Lines */}
-      <ScrollView contentContainerStyle={{ flexGrow: 2 }}
-        alwaysBounceVertical={false}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}>
-        <TouchableWithoutFeedback
-          onPress={() => inputRef.current?.focus()}
-        >
-          <View style={themed($viewSuperLinesStyle)}>
-            {Array.from({ length: lines }).map((_, i) => (
-              <View key={i} style={themed($viewLinesStyle)} />
-            ))
-            }
-          </View>
-        </TouchableWithoutFeedback>
+      {
+        //usuario escrevendo
+        acaoUsuario != 'leitura' ?
+          <ScrollView contentContainerStyle={{ flexGrow: 2 }}
+            alwaysBounceVertical={false}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}>
+            <TouchableWithoutFeedback
+              onPress={() => inputRef.current?.focus()}
+            >
+              <View style={themed($viewSuperLinesStyle)}>
+                {Array.from({ length: lines }).map((_, i) => (
+                  <View key={i} style={themed($viewLinesStyle)} />
+                ))
+                }
+              </View>
+            </TouchableWithoutFeedback>
 
-        <TextInput
-          multiline
-          ref={inputRef}
-          onContentSizeChange={() => {
-            setNewHeight(newHeight + 1)
-            if (newHeight >= LINES)
-              setLines(lines + 1);
+            <TextInput
+              multiline
+              ref={inputRef}
+              value={conteudo}
+              onChangeText={(text) => setConteudo(text)}
+              onContentSizeChange={() => {
+                setNewHeight(newHeight + 1)
+                if (newHeight >= LINES)
+                  setLines(lines + 1);
 
-          }}
-          style={themed($textInputStyle)}
-          secureTextEntry={true}
-        />
+              }}
+              style={themed($textInputStyle)}
+              secureTextEntry={true}
+            />
 
-      </ScrollView>
-      <FAB icon="plus" style={themed($fab)} onPress={() => console.log('Pressed')}></FAB>
+
+          </ScrollView>
+          : conteudo.length <= 0 ?
+            /*usuario ainda não escreveu nada! */
+            <ScrollView contentContainerStyle={{ flexGrow: 2 }}
+              alwaysBounceVertical={false}
+              showsVerticalScrollIndicator={false}
+              showsHorizontalScrollIndicator={false}>
+              <TouchableWithoutFeedback
+                onPress={() => inputRef.current?.focus()}
+              >
+                <View style={themed($viewSuperLinesStyle)}>
+                  {Array.from({ length: lines }).map((_, i) => (
+                    <View key={i} style={themed($viewLinesStyle)} />
+                  ))
+                  }
+                </View>
+              </TouchableWithoutFeedback>
+
+              <Text style={{ textAlign: 'center' }}>Nada escrito ainda!</Text>
+
+
+            </ScrollView>
+
+            : <ScrollView contentContainerStyle={{ flexGrow: 2 }}
+              alwaysBounceVertical={false}
+              showsVerticalScrollIndicator={false}
+              showsHorizontalScrollIndicator={false}>
+              <TouchableWithoutFeedback
+                onPress={() => inputRef.current?.focus()}
+              >
+                <View style={themed($viewSuperLinesStyle)}>
+                  {Array.from({ length: lines }).map((_, i) => (
+                    <View key={i} style={themed($viewLinesStyle)} />
+                  ))
+                  }
+                </View>
+              </TouchableWithoutFeedback>
+
+              <TextInput
+              editable={false}
+                multiline
+                ref={inputRef}
+                value={conteudo}
+                onChangeText={(text) => setConteudo(text)}
+                onContentSizeChange={() => {
+                  setNewHeight(newHeight + 1)
+                  if (newHeight >= LINES)
+                    setLines(lines + 1);
+
+                }}
+                style={themed($textInputStyle)}
+                secureTextEntry={true}
+              />
+
+
+            </ScrollView>
+      }
+
+      {/* FAB */}
+      <FAB icon={acaoUsuario == 'leitura' && conteudo.length <= 0 ? 'plus' : acaoUsuario == 'leitura' && conteudo.length >= 0 ? 'pencil' : 'check'} style={themed($fab)} onPress={handleFAB}></FAB>
     </Screen>
   )
 }
